39cdae0d4a2a56c06e4feccf27d3c9b4c33aa2a0
Macneyste <yoonisabdikhaadir@gmail.com>
2026-04-21 11:45:09 +0300

    adding user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please add a name'],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, 'Please add an email'],
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Please add a password'],
+      minlength: 6,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
